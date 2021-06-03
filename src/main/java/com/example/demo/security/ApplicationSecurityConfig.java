dc13dce7af0938db40d5a835f8540775b01279f6
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2021-06-03 16:21:26 -0500

    remember me cookie and extra options

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.example.demo.security.ApplicationUserPermissions.COURSE_WRITE;
 import static com.example.demo.security.ApplicationUserRoles.*;
 
@@ -42,7 +44,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin() //Form based authentication
                 .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/courses", true);
+                .defaultSuccessUrl("/courses", true)
+                .and()
+                .rememberMe()
+                     .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                     .key("secure key md5 hash");
 
 
         /*
